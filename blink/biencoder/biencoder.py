5680b2ecf3513fd51d02bf4b37ae032770875777
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2021-04-04 14:20:38 -0400

    Trying to load biobert

--- blink/biencoder/biencoder.py
+++ blink/biencoder/biencoder.py
@@ -87,7 +87,7 @@ class BiEncoderRanker(torch.nn.Module):
         # init model
         model_path = params.get("path_to_model", None)
         self.tokenizer = BertTokenizer.from_pretrained(
-            os.path.join(params["bert_model"], 'vocab.txt'), do_lower_case=params["lowercase"]
+            os.path.join(model_path, 'vocab.txt'), do_lower_case=params["lowercase"]
         )
         # self.tokenizer = BertTokenizer.from_pretrained(
         #     params["bert_model"], do_lower_case=params["lowercase"]
