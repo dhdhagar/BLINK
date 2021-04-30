4562144832be7b89e1adad396d56b18fc97d1360
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2021-04-30 11:49:25 -0400

    Fixed interfering variable names in successive loops

--- blink/biencoder/train_biencoder_mst.py
+++ blink/biencoder/train_biencoder_mst.py
@@ -355,9 +355,9 @@ def main(params):
             negative_dict_inputs = []
             negative_men_inputs = []
 
-            for i, m_embed in enumerate(mention_embeddings):
-                mention_idx = int(mention_idxs[i])
-                gold_idxs = set(train_processed_data[mention_idx]['label_idxs'][:n_gold[i]])
+            for m_embed_idx, m_embed in enumerate(mention_embeddings):
+                mention_idx = int(mention_idxs[m_embed_idx])
+                gold_idxs = set(train_processed_data[mention_idx]['label_idxs'][:n_gold[m_embed_idx]])
                 if mention_idx in gold_links:
                     gold_link_idx = gold_links[mention_idx]
                 else:
@@ -402,10 +402,10 @@ def main(params):
                 knn_dict = knn//2
                 knn_men = knn - knn_dict
                 if use_types:
-                    entity_type = train_processed_data[mention_idxs[i]]['type']
+                    entity_type = train_processed_data[mention_idxs[m_embed_idx]]['type']
                     train_dict_index = train_dict_indexes[entity_type]
                     train_men_index = train_men_indexes[entity_type]
-                _, knn_dict_idxs = train_dict_index.search(np.expand_dims(m_embed, axis=0), knn_dict + int(n_gold[i]))
+                _, knn_dict_idxs = train_dict_index.search(np.expand_dims(m_embed, axis=0), knn_dict + int(n_gold[m_embed_idx]))
                 knn_dict_idxs = knn_dict_idxs.astype(np.int64).flatten()
                 _, knn_men_idxs = train_men_index.search(np.expand_dims(m_embed, axis=0), knn_men + sum([len(train_gold_clusters[gi]) for gi in gold_idxs]))
                 knn_men_idxs = knn_men_idxs.astype(np.int64).flatten()
