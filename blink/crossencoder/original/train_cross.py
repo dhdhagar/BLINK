9dead6b346cf5787c92eddec686bb7f0722e09c0
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2022-01-08 21:11:24 +0530

    Added changes to take the union of all biencoder candidate sets for oracle cross-encoder experiment

--- blink/crossencoder/original/train_cross.py
+++ blink/crossencoder/original/train_cross.py
@@ -243,7 +243,7 @@ def get_data_loader(data_split, tokenizer, context_length, candidate_length, max
                     logger, inject_ground_truth=False, max_n=None, shuffle=True, return_data=False):
     # Load the top-64 indices for each mention query and the ground truth label if it exists in the candidate set
     logger.info(f"Loading {data_split} data...")
-    fname = os.path.join(params["biencoder_indices_path"], f"candidates_{data_split}_top64.t7")  # train.t7
+    fname = os.path.join(params["biencoder_indices_path"], f"candidates_{data_split}_top64.t7")
     stored_data = torch.load(fname)
     entity_dictionary, tensor_data, processed_data = load_data(data_split,
                                                                tokenizer,
@@ -344,7 +344,8 @@ def main(params):
     candidate_length = params["max_context_length"]
 
     if params["only_evaluate"]:
-        test_dataloader, n_test_skipped, data = get_data_loader('test', tokenizer, context_length, candidate_length,
+        split = "test" if params["custom_cand_set"] is None else params["custom_cand_set"]
+        test_dataloader, n_test_skipped, data = get_data_loader(split, tokenizer, context_length, candidate_length,
                                                                 max_seq_length, pickle_src_path, logger,
                                                                 inject_ground_truth=params["inject_eval_ground_truth"],
                                                                 shuffle=False, return_data=True)
